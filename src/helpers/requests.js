1e0b69278fcd572a8ddf4555dd9efe3d260a0437
omar-he <omar.hendi92@gmail.com>
2021-07-27 11:52:57 +0300

    style fixes

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,6 +1,6 @@
 const clientID = "cxfEBYtywoPwbNS0RmkunuvdhxR0qxF1wtFTdWDj1Bc";
 
-export async function getImages(pageNumber = "1", searchTerm = "cat") {
+export async function getImages(pageNumber = "1", searchTerm = "nature") {
   const req = fetch(
     `https://api.unsplash.com/search/photos?page=${pageNumber}&query=${searchTerm}&client_id=${clientID}`
   );
